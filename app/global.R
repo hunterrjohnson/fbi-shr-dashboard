99ea680032e998a372a8fbde334eb727ae35a4ad
Hunter Johnson <44483714+hunterrjohnson@users.noreply.github.com>
2020-11-24 17:30:31 -0700

    Update global.R
    Extend dashboard to include weapon, victim/offender relation, and circumstances

--- app/global.R
+++ app/global.R
@@ -3,12 +3,12 @@
 lapply(c('data.table','dplyr','ggplot2','shiny','shinydashboard',
          'shinythemes','leaflet','DT','plotly','scales'), library, character.only = TRUE)
 
-#setwd('/Users/hunterjohnson/Desktop/Dashboards/US Crime/Supplementary Homicide Reports/')
+source('R/App/functions.R')
 
 #====================================================================================================
 # Read in data
 
-# Main SHR data (long for plotly, wide for datatable)
+# Offender/victim data (long for plotly, wide for datatable)
 main_df_long <- data.frame(fread('Input/shr_analysis_data_long.csv'))
 main_df_wide <- data.frame(fread('Input/shr_analysis_data_wide.csv'), check.names = FALSE) # Keep invalid names
 
@@ -20,11 +20,17 @@ main_df_victims <- main_df_long[grep('Victims', main_df_long$Variable),]
 main_df_victims$Variable <- gsub(' Victims', '', main_df_victims$Variable)
 main_df_victims$Variable <- gsub('Victims, ', '', main_df_victims$Variable)
 
-# U.S. states shapefile
-states_shp <- sf::read_sf('Input/us_shapefiles/cb_2018_us_state_500k.shp')
-# Remove territories
-states_shp <- states_shp[-which(states_shp$NAME %in% c('American Samoa','Puerto Rico','United States Virgin Islands',
-                                                       'Guam','Commonwealth of the Northern Mariana Islands')), ]
+# Circumstances data
+circum_df <- data.frame(fread('Input/shr_circumstances_analysis_data_long.csv'))
+circum_df_wide <- data.frame(fread('Input/shr_circumstances_analysis_data_wide.csv'), check.names = FALSE)
+
+# Weapons data
+weapon_df <- data.frame(fread('Input/shr_weapon_analysis_data_long.csv'))
+weapon_df_wide <- data.frame(fread('Input/shr_weapon_analysis_data_wide.csv'), check.names = FALSE)
+
+# Reasons data
+reason_df <- data.frame(fread('Input/shr_reason_analysis_data_long.csv'))
+reason_df_wide <- data.frame(fread('Input/shr_reason_analysis_data_wide.csv'), check.names = FALSE)
 
 # Census data
 census_df <- data.frame(fread('Input/census_data.csv'))
@@ -32,23 +38,55 @@ census_df <- data.frame(fread('Input/census_data.csv'))
 #====================================================================================================
 # Create arrays of Variables for use in UI and server
 
-# Order Variables for selectizeInput
-plotly_input_vars_both <- c('Total Offenders','Total Victims','Male Offenders','Male Victims','Female Offenders','Female Victims',
-                            'Black Offenders','Black Victims','White Offenders','White Victims',
-                            'Asian/Pacific Islander Offenders','Asian/Pacific Islander Victims',
-                            'Native American Offenders','Native American Victims','Black Male Offenders','Black Male Victims',
-                            'Black Female Offenders','Black Female Victims','White Male Offenders','White Male Victims',
-                            'White Female Offenders','White Female Victims',
-                            'Asian/Pacific Islander Male Offenders','Asian/Pacific Islander Male Victims',
-                            'Asian/Pacific Islander Female Offenders','Asian/Pacific Islander Female Victims',
-                            'Native American Male Offenders','Native American Male Victims',
-                            'Native American Female Offenders','Native American Female Victims')
-plotly_input_vars_race <- c('Total Offenders','Total Victims','Black Offenders','Black Victims','White Offenders','White Victims',
-                            'Asian/Pacific Islander Offenders','Asian/Pacific Islander Victims',
-                            'Native American Offenders','Native American Victims')
-plotly_input_vars_sex <- c('Total Offenders','Total Victims','Male Offenders','Male Victims','Female Offenders','Female Victims')
+# Order variables for selectizeInput - demographics tab
+trend_inp_vars_demog_both <- c('Total Offenders','Total Victims','Male Offenders','Male Victims','Female Offenders','Female Victims',
+                               'Black Offenders','Black Victims','White Offenders','White Victims',
+                               'Asian/Pacific Islander Offenders','Asian/Pacific Islander Victims',
+                               'Native American Offenders','Native American Victims','Black Male Offenders','Black Male Victims',
+                               'Black Female Offenders','Black Female Victims','White Male Offenders','White Male Victims',
+                               'White Female Offenders','White Female Victims',
+                               'Asian/Pacific Islander Male Offenders','Asian/Pacific Islander Male Victims',
+                               'Asian/Pacific Islander Female Offenders','Asian/Pacific Islander Female Victims',
+                               'Native American Male Offenders','Native American Male Victims',
+                               'Native American Female Offenders','Native American Female Victims')
+trend_inp_vars_demog_race <- c('Total Offenders','Total Victims','Black Offenders','Black Victims','White Offenders','White Victims',
+                               'Asian/Pacific Islander Offenders','Asian/Pacific Islander Victims',
+                               'Native American Offenders','Native American Victims')
+trend_inp_vars_demog_sex <- c('Total Offenders','Total Victims','Male Offenders','Male Victims','Female Offenders','Female Victims')
 
-# Ordered variables for pie chart selections
+# Order variables for selectizeInput - victim/offender tab
+trend_inp_vars_vic_off <- c('Total Offenses','Friend','Acquaintance','Neighbor','Stranger','Employer','Employee',
+                            'Girlfriend','Boyfriend','Wife','Husband','Homosexual Partner','Ex-Wife','Ex-Husband',
+                            'Common-Law Wife','Common-Law Husband','Daughter','Son','Sister','Brother','Mother','Father',
+                            'Stepdaughter','Stepson','Stepmother','Stepfather','In-Law','Other Family','Other','Unknown')
+bar_inp_vars_vic_off <- c('Friend','Acquaintance','Neighbor','Stranger','Employer','Employee',
+                          'Girlfriend','Boyfriend','Wife','Husband','Homosexual Partner','Ex-Wife','Ex-Husband',
+                          'Common-Law Wife','Common-Law Husband','Daughter','Son','Sister','Brother','Mother','Father',
+                          'Stepdaughter','Stepson','Stepmother','Stepfather','In-Law','Other Family','Other','Unknown')
+
+# Order variables for selectizeInput - weapon tab
+trend_inp_vars_weap <- c('Total Offenses','Handgun','Shotgun','Rifle','Firearm, Other','Firearm, Type Unknown',
+                         'Personal Weapons (Includes Beating)','Blunt Object','Strangulation','Drowning',
+                         'Pushed/Thrown Out of Window','Knife/Cutting Instrument','Asphyxiation',
+                         'Poison','Narcotics/Drugs','Fire','Explosives','Other')
+bar_inp_vars_weap_bar <- c('Handgun','Shotgun','Rifle','Firearm, Other','Firearm, Type Unknown',
+                           'Personal Weapons (Includes Beating)','Blunt Object','Strangulation','Drowning',
+                           'Pushed/Thrown Out of Window','Knife/Cutting Instrument','Asphyxiation',
+                           'Poison','Narcotics/Drugs','Fire','Explosives','Other')
+
+# Order variables for selectizeInput - circumstances tab
+trend_inp_vars_circ <- c('Total Offenses','Argument Over Money/Property','Robbery','Burglary','Larceny','Motor Vehicle Theft',
+                         'Felon Killed by Police','Felon Killed by Private Citizen',"Lovers' Triangle",'Brawl Involving Alcohol','Brawl Involving Narcotics',
+                         'Gangland Killing','Juvenile Gang Killing','Rape','Prostitution/Commercialized Vice','Other Sex Offense',
+                         'Gambling','Narcotic/Drug Laws','Arson','Institutional Killing','Sniper Attack','Child Killed by Babysitter',
+                         'All Suspected Felony Type','Other Felony, Not Specified','Other Non-Felony, Not Specified','Unknown')
+bar_inp_vars_circ <- c('Argument Over Money/Property','Robbery','Burglary','Larceny','Motor Vehicle Theft',
+                       'Felon Killed by Police','Felon Killed by Private Citizen',"Lovers' Triangle",'Brawl Involving Alcohol','Brawl Involving Narcotics',
+                       'Gangland Killing','Juvenile Gang Killing','Rape','Prostitution/Commercialized Vice','Other Sex Offense',
+                       'Gambling','Narcotic/Drug Laws','Arson','Institutional Killing','Sniper Attack','Child Killed by Babysitter',
+                       'All Suspected Felony Type','Other Felony, Not Specified','Other Non-Felony, Not Specified','Unknown')
+
+# Ordered Variables for pie chart selections
 customOrder_census_race <- c('Black','White','Asian/Pacific Islander','Native American','Other')
 customOrder_off_race <- c('Black','White','Asian/Pacific Islander','Native American','Race Unknown')
 customOrder_vic_race <- c('Black','White','Asian/Pacific Islander','Native American','Race Unknown')
@@ -64,31 +102,6 @@ customOrder_off_both <- c('Black Male','Black Female','White Male','White Female
 customOrder_vic_both <- c('Black Male','Black Female','White Male','White Female',
                           'Native American Male','Native American Female','Asian/Pacific Islander Male',
                           'Asian/Pacific Islander Female','Male, Race Unknown','Female, Race Unknown')
-
-#====================================================================================================
-# Leaflet map
-
-# Popup labels
-labels <- sprintf(
-  "<strong>%s</strong>",
-  states_shp$NAME
-) %>% lapply(htmltools::HTML)
-
-pal <- colorFactor(
-  palette = c('red', 'blue', 'green', 'purple', 'orange'),
-  domain = states_shp$NAME
-)
-
-# Map
-leaf_map <- leaflet(states_shp) %>%
-  addProviderTiles(providers$OpenStreetMap) %>%
-  addPolygons(layerId = ~NAME,
-              weight = 1,
-              opacity = 0.6,
-              color = 'black',
-              fillColor = ~pal(NAME),
-              label = labels) %>%
-  setView(lat = 39.50, lng = -98.35, zoom = 3) # US Centroid
 
 
 
